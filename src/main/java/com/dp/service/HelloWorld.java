c002517e8134089deecddeb8fd97e4a1f1c52283
ananddeshmane <ananddeshmane9@gmail.com>
2019-05-15 18:03:33 +0530

    hello world with test

--- src/main/java/com/dp/service/HelloWorld.java
+++ src/main/java/com/dp/service/HelloWorld.java
@@ -3,7 +3,7 @@ package com.dp.service;
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("HELLO WORLD !!!");
+		System.out.println("HELLO WORLD working");
 	}
 
 }
---
+++ src/main/java/com/dp/service/TestHelloWorld.java
@@ -0,0 +1,9 @@
+package com.dp.service;
+
+public class TestHelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Test HELLO WORLD working");
+	}
+
+}
